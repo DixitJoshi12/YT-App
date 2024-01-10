5a8165cc96789077741cac75b55f053e6adef4c3
Dixit Joshi <dixitjoshi@192.168.1.16>
2024-01-11 00:06:57 +0530

    added basic controller

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
-import cookieParser from "cookie-parser";
+import cookieParser from 'cookie-parser';
+
 
 const app = express();
 
@@ -12,6 +13,13 @@ app.use(express.json({
 }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
-app.use(express.cookieParser());
+app.use(cookieParser());
+
+
+// importing routes
+import  userRouter from "./routes/user.routes.js";
+
+// routes declaration
+app.use("/api/v1/users",userRouter);
 
 export { app };
--- src/db/index.js
+++ src/db/index.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import { DB_Name } from "../constants.js";
 
+// connecting to the mongo db
 const connectDB = async () =>{
     try{
       const connectionInstance =  await mongoose.connect(`${process.env.MONGODB_URI}/${DB_Name}`);
