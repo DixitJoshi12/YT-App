121d9feaf4bd7ce679795d282d40a29f74c135db
Dixit Joshi <dixitjoshi@192.168.1.3>
2024-01-27 23:32:28 +0530

    added all user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,18 @@
-import { registerUser , loginUser , logOutUser, refreshAccessToken} from "../controllers/user.controller.js";
+import { 
+    registerUser,
+    loginUser,
+    logOutUser, 
+    refreshAccessToken, 
+    changeCurrentPassword, 
+    getCurrentUser, 
+    updateAccountDetails, 
+    updateUserAvatar, 
+    updateUserCoverImage, 
+    getUserChannelProfile, 
+    getWatchHistory 
+} from "../controllers/user.controller.js";
 import { Router } from "express";
-import {upload} from "../middlewares/multer.middleware.js";
+import { upload } from "../middlewares/multer.middleware.js";
 import { jwtValidator } from "../middlewares/auth.middleware.js";
 
 const router = Router();
@@ -8,21 +20,36 @@ const router = Router();
 router.route("/register").post(
     upload.fields([
         {
-            name : "avatar",
-            maxCount : 1
+            name: "avatar",
+            maxCount: 1
         },
         {
-            name : "coverImage",
-            maxCount : 1
+            name: "coverImage",
+            maxCount: 1
         }
     ]),
     registerUser);
 
-    router.route("/login").post(loginUser);
+router.route("/login").post(loginUser);
 
-    //secured route
-    router.route("/logout").post(jwtValidator,logOutUser);
-    router.route("/refresh-token").post(refreshAccessToken);
+//secured route
+router.route("/logout").post(jwtValidator, logOutUser);
+
+router.route("/refresh-token").post(refreshAccessToken);
+
+router.route("/changed-password").post(jwtValidator, changeCurrentPassword);
+
+router.route("/current-user").get(jwtValidator, getCurrentUser);
+
+router.route("/update-account").patch(jwtValidator, updateAccountDetails);
+
+router.route("/avatar").patch(jwtValidator, upload.single("avatar"), updateUserAvatar);
+
+router.route("/coverImage").patch(jwtValidator, upload.single("coverImage"), updateUserCoverImage);
+
+router.route("/c/:userName").get(jwtValidator, getUserChannelProfile);
+
+router.route("/history").get(jwtValidator, getWatchHistory);
 
 export default router;
 
